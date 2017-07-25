91cc9fecb2537d43a61dc322641a60cf22a590b0
doomtr666 <christophe.delacourt.perso@gmail.com>
2017-07-25 08:12:55 +0200

    Add VS2017 libs

--- build.ps1
+++ build.ps1
@@ -6,19 +6,38 @@ git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
 git clone https://github.com/KhronosGroup/SPIRV-Headers.git spirv-tools/external/spirv-headers
 cd ..\..
 
-# cmake x86
+
+# MSBuild workaround
+$Env:PATH=$Env:PATH+";C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\"
+
+# cmake VS2015 x86
 mkdir shaderc_v140_x86
 cd shaderc_v140_x86
-cmake -G "Visual Studio 14" ..\shaderc -DSHADERC_ENABLE_SHARED_CRT:bool=true
+cmake -G "Visual Studio 14"  -DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_TESTS=ON ..\shaderc
 cmake --build . --config release
 cmake --build . --config debug
 cd ..
 
-# cmake x64
+# cmake VS2015 x64
 mkdir shaderc_v140_x64
 cd shaderc_v140_x64
-cmake -G "Visual Studio 14 Win64" ..\shaderc -DSHADERC_ENABLE_SHARED_CRT:bool=true
+cmake -G "Visual Studio 14 Win64" -DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_TESTS=ON ..\shaderc 
 cmake --build . --config release
 cmake --build . --config debug
 cd ..
 
+# cmake VS2017 x86
+mkdir shaderc_v141_x86
+cd shaderc_v141_x86
+cmake -G "Visual Studio 15" -DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_TESTS=ON ..\shaderc 
+cmake --build . --config release
+cmake --build . --config debug
+cd ..
+
+# cmake VS2017 x64
+mkdir shaderc_v141_x64
+cd shaderc_v141_x64
+cmake -G "Visual Studio 15 Win64" -DSHADERC_ENABLE_SHARED_CRT=ON -DBUILD_TESTING=OFF -DSPIRV_SKIP_TESTS=ON ..\shaderc 
+cmake --build . --config release
+cmake --build . --config debug
+cd ..
